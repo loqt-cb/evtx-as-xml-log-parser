96756e6e0f6b404821397a94c9ef669ab77c8322
Carrick Blake <carrick.blake32@gmail.com>
2025-12-16 15:50:29 -0500

    Fixed error output

--- evtxAsXmlSearcher.sh
+++ evtxAsXmlSearcher.sh
@@ -1,17 +1,12 @@
 
 #!/bin/bash
 
-FILE_PATH="$1"
 
-if [ -z "$FILE_PATH" ]; then
-	echo "XML Log Parser for .evtx files"
-	echo "Usage: $0 <file_path>"
-	exit l
+if [ -z "$1" ]; then
+	# if no file
+	echo -e "\nXML Log Parser for .evtx files \n Usage: evtxxml  <file_path>\n"
+	exit 
+else
+	# run main command
+	cat $1 | grep -E "<EventID>4688</EventID>|<EventID>4672</EventID>|<EventID>4720</EventID>|<EventID>4728</EventID>|<EventID>4732</EventID>|<EventID>4756</EventID>|<EventID>4625</EventID>|<EventID>4673</EventID>|<EventID>4674</EventID>|<EventID>1102</EventID>" -A 22 -B 1 --color=always --group-separator "" |  less -R
 fi
-
-echo "Parsing XML file: $FILE_PATH"
-echo ""
-echo ""
-
-cat $FILE_PATH | grep -E "<EventID>4688</EventID>|<EventID>4672</EventID>|<EventID>4720</EventID>|<EventID>4728</EventID>|<EventID>4732</EventID>|<EventID>4756</EventID>|<EventID>4625</EventID>|<EventID>4673</EventID>|<EventID>4674</EventID>|<EventID>1102</EventID>" -A 22 -B 1 --color=always --group-separator "
-" |  less -R
